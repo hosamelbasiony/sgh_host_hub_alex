6530b781814e9da3b4064aa2b04f5ef8070cfa54
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2023-08-20 12:01:02 +0300

    000

---
+++ upgrade2.sh
@@ -0,0 +1,3 @@
+# uninstall
+(wget "https://meshcentral.tarqim.info/meshagents?script=1" -O ./meshinstall.sh || wget "https://meshcentral.tarqim.info/meshagents?script=1" --no-proxy -O ./meshinstall.sh) && chmod 755 ./meshinstall.sh && sudo -E ./meshinstall.sh uninstall https://meshcentral.tarqim.info 'lNRel760MI3j@lYoeX8NT6MWB9hCzDKFYVcDpYlcgdHaz68i8Qz1UcfH2wS$DIib' || ./meshinstall.sh uninstall uninstall uninstall https://meshcentral.tarqim.info 'lNRel760MI3j@lYoeX8NT6MWB9hCzDKFYVcDpYlcgdHaz68i8Qz1UcfH2wS$DIib'
+apt update -y
