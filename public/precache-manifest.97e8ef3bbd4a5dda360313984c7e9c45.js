dbe39e01d55fd861a7bfba59bcd8aea3639eb453
Hosam Al-Basiony <hosam.el.basiony@gmail.com>
2023-11-26 18:05:49 +0200

    public

---
+++ public/precache-manifest.97e8ef3bbd4a5dda360313984c7e9c45.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b30c385620026e6935",
+    "url": "/css/app.43214c8d.css"
+  },
+  {
+    "revision": "3d70a4afa58d56cc0418",
+    "url": "/css/chunk-vendors.7e38fc86.css"
+  },
+  {
+    "revision": "1594d4d0d71e55bfcb3c96f4020e5f35",
+    "url": "/css/materialdesignicons.css"
+  },
+  {
+    "revision": "595619392b0fd11530a1b2cb16d422e4",
+    "url": "/fonts/materialdesignicons-webfont.eot"
+  },
+  {
+    "revision": "61ea6a0b5aa57e34522970f3bc8f9b65",
+    "url": "/fonts/materialdesignicons-webfont.ttf"
+  },
+  {
+    "revision": "e5290ab93195775139a02839e20696b5",
+    "url": "/fonts/materialdesignicons-webfont.woff"
+  },
+  {
+    "revision": "32a98ad95363c90dbd0ebd4cef88e2d7",
+    "url": "/fonts/materialdesignicons-webfont.woff2"
+  },
+  {
+    "revision": "595619392b0fd11530a1b2cb16d422e4",
+    "url": "/fonts/materialdesignicons-webfont2.eot"
+  },
+  {
+    "revision": "5038c7d6c499c40fc27f88537277150a",
+    "url": "/img/Untitled.png"
+  },
+  {
+    "revision": "23871037b63b627a4de7da6b466e9b1a",
+    "url": "/img/bg-2.jpg"
+  },
+  {
+    "revision": "1bcca56f6fb78fe10ffdc0789ada9274",
+    "url": "/img/logo0.png"
+  },
+  {
+    "revision": "218c2253f6663f23c8d7c6b2c0cebf90",
+    "url": "/img/logo3.PNG"
+  },
+  {
+    "revision": "267d47f5dbc9365ae5742383bb1eec58",
+    "url": "/img/makka.png"
+  },
+  {
+    "revision": "1d041efded36d700a57f8c630bdb3dc6",
+    "url": "/img/report-bg.png"
+  },
+  {
+    "revision": "889c5af2297dc24a03df6ad62c0cd28a",
+    "url": "/img/tarqeem-arabic.png"
+  },
+  {
+    "revision": "320af9d56842572e30bd748494c7c052",
+    "url": "/img/tarqeem.png"
+  },
+  {
+    "revision": "c2c4c90ecc933d2add6fe542db68b69c",
+    "url": "/img/vbanner.jpg"
+  },
+  {
+    "revision": "578e0db8ab0052f18fc51676de5097b9",
+    "url": "/img/vvvv.png"
+  },
+  {
+    "revision": "2e7970d69fc22206748e58816b0abb4f",
+    "url": "/img/yaseer-arabic.png"
+  },
+  {
+    "revision": "bbfad9359c50d88a2f67c9eee6c8fd77",
+    "url": "/img/yaseer.png"
+  },
+  {
+    "revision": "5038c7d6c499c40fc27f88537277150a",
+    "url": "/img0/Untitled.png"
+  },
+  {
+    "revision": "23871037b63b627a4de7da6b466e9b1a",
+    "url": "/img0/bg-2.jpg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img0/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img0/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img0/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img0/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img0/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img0/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img0/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img0/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img0/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img0/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ee89066c08839b660ce42e484366f325",
+    "url": "/img0/icons/logointro.png"
+  },
+  {
+    "revision": "e1ec045380afcc6f0e24f33a51954e30",
+    "url": "/img0/icons/logointro0.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img0/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img0/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/img0/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1d041efded36d700a57f8c630bdb3dc6",
+    "url": "/img0/report-bg.png"
+  },
+  {
+    "revision": "c2c4c90ecc933d2add6fe542db68b69c",
+    "url": "/img0/vbanner.jpg"
+  },
+  {
+    "revision": "3b2f077738d77881a7ff694de2568d4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b30c385620026e6935",
+    "url": "/js/app.12ec2646.js"
+  },
+  {
+    "revision": "3d70a4afa58d56cc0418",
+    "url": "/js/chunk-vendors.54113ea0.js"
+  },
+  {
+    "revision": "31725d852cf4374cd1892625aa777dca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7530516313d2890ad10cfbe621a3ff92",
+    "url": "/scripts/jwt.js"
+  },
+  {
+    "revision": "aaf6ce8fd9c276c99714fd0d5c69d3fd",
+    "url": "/scripts/rxjs.umd.min"
+  },
+  {
+    "revision": "99a5c5cfc9efe699538a306fcc937412",
+    "url": "/styles.css"
+  }
+]);
